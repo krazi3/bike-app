76ed6a886e3833be2eaecaab15c2e0d84f59ef0c
Awwab Ul Haq <awwabhaq@gmail.com>
2017-03-22 21:10:48 +0500

    Added in bootstrap css for styling

--- client/app.jsx
+++ client/app.jsx
@@ -2,4 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main.jsx';
 
+import 'bootstrap/dist/css/bootstrap.css';
+
 ReactDOM.render(<Main />, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader', query: {presets: ["react", "latest", "stage-0"]} },
+      { test: /\.css$/, loader: 'style-loader!css-loader' },
+      { test: /\.(eot|svg|ttf|woff|woff2)\?*.*$/, loader: 'file-loader'},
     ]
   },
   devServer: {
